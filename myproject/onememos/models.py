3caa49e20ca5c8dfef104bd307f0b5e157c30ae2
yesyell <yelin5262@gmail.com>
2023-01-10 22:38:50 +0900

    테이블 생성하기

--- myproject/onememos/models.py
+++ myproject/onememos/models.py
@@ -9,6 +9,6 @@ from django.db import models
 
 class Memo(models.Model):
     memo_text = models.CharField(max_length = 200) # 문자형
-    published_date = models.DateTimeField() # 날짜형
+    published_date = models.DateTimeField(auto_now_add = True) # 날짜형
 
 # 2. 작성 후 admin 사이트에 반영 - admin.py 추가 작성
