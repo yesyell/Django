10b3c893f665b235727985c4f74d6c2ad65c527c
yesyell <yelin5262@gmail.com>
2023-01-16 21:03:01 +0900

    뷰페이지 템플릿 만들기 - 관리자 모드에서 DB 조작

--- myproject/onememos/models.py
+++ myproject/onememos/models.py
@@ -6,9 +6,11 @@ from django.db import models
 # idx(정수형)              X
 # memo_text(문자형)        O
 # published_date(날짜형)   O
+# 2. 작성 후 admin 사이트에 반영 - admin.py 추가 작성
 
 class Memo(models.Model):
     memo_text = models.CharField(max_length = 200) # 문자형
     published_date = models.DateTimeField(auto_now_add = True) # 날짜형
 
-# 2. 작성 후 admin 사이트에 반영 - admin.py 추가 작성
+    def __str__(self):
+        return self.memo_text
--- myproject/onememos/views.py
+++ myproject/onememos/views.py
@@ -19,13 +19,13 @@ from .models import *
 #   - python manage.py runserver 서버 재시작 ⭐️
 
 def main(request):
-    #return HttpResponse("Onememeos~ Hello, World~^.~")
+    # return HttpResponse("Onememeos~ Hello, World~^.~")
+    # return render(request, 'onememos/templates/main.html') X
+    # return render(request, 'onememos/main.html') X
     return render(request, 'main.html')
-
 
 def createMemo(request):
     # request 객체는 사용자가 폼 페이지를 통해 입력한 폼 데이터 값들을 받는다.
-    # request.GET, request.POST, request.COOKIE
-    # -> 사전형 데이터 -> get, post, cookie 정보들을 받는다.
+    # request.GET, request.POST, request.COOKIE -> 사전형 데이터 -> get, post, cookie 정보들을 받는다.
     memoContent = request.GET['memoContent']
     return HttpResponse("Create Memo = " + memoContent)
