e7a7935fab7258ecb818c3c28917d91a519bc123
yesyell <yelin5262@gmail.com>
2023-01-17 22:06:53 +0900

    뷰페이지 템플릿 만들기 - Form 작성 및 DB 입력과 출력

--- myproject/onememos/views.py
+++ myproject/onememos/views.py
@@ -27,5 +27,9 @@ def main(request):
 def createMemo(request):
     # request 객체는 사용자가 폼 페이지를 통해 입력한 폼 데이터 값들을 받는다.
     # request.GET, request.POST, request.COOKIE -> 사전형 데이터 -> get, post, cookie 정보들을 받는다.
-    memoContent = request.GET['memoContent']
+
+    # memoContent = request.GET['memoContent']
+    memoContent = request.POST['memoContent']
+
     return HttpResponse("Create Memo = " + memoContent)
+    # localhost:8000/onememos/createMemo/?memoContent=대한민국
